2dd8e69bc0f86c9f8e241061b41cbb1253f2b1e4
Daniel galvan <danielricardogt@gmail.com>
2025-06-01 22:20:56 -0500

    :wrench: customHooks

---
+++ src/02-use-effect/SimpleFormWithCustomHook.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react"
+import { UseForm } from "../hooks/UseForm";
+
+
+
+export const SimpleFormWithCustonHook = () => {
+  
+
+    const {FormState, onInputChange, username, email, password} = UseForm(    {
+        username: "",
+        email: "",
+        password: "",
+      })
+
+
+ 
+
+    return (
+        <>
+            <h1>Formulario wit custom Hook</h1>
+
+            <br />
+
+            <input type="text" className="form-control" placeholder="username" name="username" value={username} onChange={onInputChange}
+            />
+
+            <input type="email" className="form-control mt-2" placeholder="email" name="email" value={email} onChange={onInputChange}
+            />
+
+            <input type="password" className="form-control mt-2" placeholder="password" name="password" value={password} onChange={onInputChange}
+            />
+
+        </>
+    )
+}
