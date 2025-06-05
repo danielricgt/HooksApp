ead43e7bc18a0014036f656665d6551747f196f6
Daniel galvan <danielricardogt@gmail.com>
2025-06-04 22:45:38 -0500

    :wrench: customHooks part III

---
+++ src/02-use-effect/IsLoading.jsx
@@ -0,0 +1,10 @@
+
+
+export const IsLoading = () => {
+  return (
+   <section>
+
+<h3 style={{height: 200}} className="alert  alert-info text-center p-0 m-0 d-flex align-items-center justifiy-content-center">Is Loading</h3>
+   </section>
+  )
+}
