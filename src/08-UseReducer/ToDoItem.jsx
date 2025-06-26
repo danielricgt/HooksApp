771265e444e211f15eb629464a81c3ff5a143aa2
Daniel galvan <danielricardogt@gmail.com>
2025-06-25 22:49:50 -0500

    :wrench: ToDoList part III

--- src/08-UseReducer/ToDoItem.jsx
+++ src/08-UseReducer/ToDoItem.jsx
@@ -1,10 +1,14 @@
 
-export const ToDoItem = ({todo}) => {
+export const ToDoItem = ({todo, onDeleteTodo}) => {
+
+  
   return (
    
     <li className="list-group-item d-flex justify-content-between">
     <span className="align-self-center">{todo.description}</span>
-    <button className="btn btn-primary">Borrar</button>
+    <button className="btn btn-primary"
+    onClick={() => onDeleteTodo(todo.id)}
+    >Borrar</button>
 
 </li>
   )
