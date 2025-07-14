f85e57a828e562f0cbccd509445028b6cbe25c7e
Daniel galvan <danielricardogt@gmail.com>
2025-07-13 22:24:03 -0500

    :test_tube:  test custom hook use counter part IIi

--- test/examples/MultipleCustomHooks.test.jsx
+++ test/examples/MultipleCustomHooks.test.jsx
@@ -1,19 +1,77 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { MultipleCustomHooks } from "../../src/03-examples/MultipleCustomHooks";
-import "isomorphic-fetch"; 
-import { renderHook } from "@testing-library/react-hooks"
+import "isomorphic-fetch";
+import { useFetch } from "../../src/hooks/useFetch";
+import { useCounter } from "../../src/hooks/UseCounter";
+jest.mock('../../src/hooks/useFetch');
+jest.mock('../../src/hooks/UseCounter');
 
-describe('multiple custom hook test', () => { 
-    test('should return default values', () => { 
+
+describe('multiple custom hook test', () => {
+
+    const incrementFunctionMock = jest.fn();
+    useCounter.mockReturnValue({
+        counter: 1,
+        increment: incrementFunctionMock,
+    });
+
+beforeEach(() => {
+    jest.clearAllMocks();
+});;
+
+
+    test('should return default values', () => {
+        useFetch.mockReturnValue({
+            data: null,
+            isLoading: true,
+            hasError: false,
+            errorMessage: null,
+        });
+
         render(<MultipleCustomHooks />);
-        screen.debug();
+
 
         expect(screen.getByText('Is Loading'));
         expect(screen.getByText('Pokemon Information'));
 
-        const nextButton = screen.getByRole('button',{name: 'Next'});
-        console.log(nextButton.disabled);
+        const nextButton = screen.getByRole('button', { name: 'Next' });
+
 
         expect(nextButton.disabled).toBeFalsy();
-     })
- })
+    });
+
+    test('should show a pokemon', () => {
+        useFetch.mockReturnValue({
+            data: [{}],
+            isLoading: true,
+            hasError: false,
+            errorMessage: null,
+        });
+        render(<MultipleCustomHooks />);
+        expect(screen.getAllByText('Pokemon Information')).toBeTruthy();
+        expect(screen.getAllByText('Is Loading')).toBeTruthy();
+        const nextButton = screen.getByRole('button', { name: 'Next' });
+        expect(nextButton.disabled).toBeFalsy();
+
+    });
+
+    test('should call increment function', () => {
+
+
+        useFetch.mockReturnValue({
+            data: null,
+            isLoading: true,
+            hasError: false,
+            errorMessage: null,
+        });
+
+
+        render(<MultipleCustomHooks />);
+        const nextButton = screen.getByRole('button', { name: 'Next' });
+        fireEvent.click(nextButton);
+        expect(incrementFunctionMock).toHaveBeenCalled();
+    });
+
+
+
+})
